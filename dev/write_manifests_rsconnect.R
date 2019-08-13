d8414613feca2073e2ad53400afeb1f86fd46c69
Kelly N. Bodwin <kbodwin@calpoly.edu>
2019-08-13 13:31:52 -0700

    regorganize for easy github installation

---
+++ dev/write_manifests_rsconnect.R
@@ -0,0 +1,9 @@
+rsconnect::writeManifest(
+  appDir = "./inst/apps/Network_Individuals/",
+  appPrimaryDoc = "Network_Individuals.R"
+)
+
+rsconnect::writeManifest(
+  appDir = "./inst/apps/Network_Intitutions/",
+  appPrimaryDoc = "Network_Institutions.R"
+)
